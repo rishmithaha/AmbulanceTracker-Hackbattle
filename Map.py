215bc93f82b707ce3c77b5e89d81204047bb49f2
Rishmitha <137617196+rishmithaha@users.noreply.github.com>
2024-09-25 16:54:12 +0530

    Update Map.py

--- Map.py
+++ Map.py
@@ -26,7 +26,7 @@ def button():
 
 # Add a button using customtkinter
 button = customtkinter.CTkButton(master=window, text="Testing", command=button)
-button.place(relx=0.5, rely=0.9, anchor=customtkinter.CENTER)
+button.place(relx=0.5, rely=0.9, anchor=customtkinter.E, size = 50)
 
 # Start the main event loop
 window.mainloop()
